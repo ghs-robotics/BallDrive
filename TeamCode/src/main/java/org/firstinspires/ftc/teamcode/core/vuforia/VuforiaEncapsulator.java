a6fab3a8ce6a2bf08b69d04298c419e2315f3efa
Moses Miller <Majora320@gmail.com>
2018-11-27 17:24:55 -0800

    Encapsulate Vuforia with the Sensor/SensorManager system

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/vuforia/VuforiaEncapsulator.java
@@ -0,0 +1,400 @@
+/* Copyright (c) 2018 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.core.vuforia;
+
+import android.hardware.Sensor;
+import android.util.Log;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.teamcode.core.ClassHolder;
+import org.firstinspires.ftc.teamcode.core.OpModeExtended;
+import org.firstinspires.ftc.teamcode.core.SensorInterface;
+import org.firstinspires.ftc.teamcode.core.SensorManager;
+import org.firstinspires.ftc.teamcode.core.Subsystem;
+import org.majora320.tealisp.evaluator.JavaInterface;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Vector;
+
+import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.DEGREES;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.XYZ;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.YZX;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesReference.EXTRINSIC;
+import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.BACK;
+import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.FRONT;
+
+/**
+ * This class encapsulates Vuforia into a simple object that is easy to use
+ * The code is structured as an Encapsulated class
+ *
+ * Vuforia uses the phone's camera to inspect it's surroundings, and attempt to locate target images.
+ *
+ * This example assumes a "square" field configuration where the red and blue alliance stations
+ * are on opposite walls of each other.
+ *
+ * From the Audience perspective, the Red Alliance station is on the right and the
+ * Blue Alliance Station is on the left.
+
+ * The four vision targets are located in the center of each of the perimeter walls with
+ * the images facing inwards towards the robots:
+ *     - BlueRover is the Mars Rover image target on the wall closest to the blue alliance
+ *     - RedFootprint is the Lunar Footprint target on the wall closest to the red alliance
+ *     - FrontCraters is the Lunar Craters image target on the wall closest to the audience
+ *     - BackSpace is the Deep Space image target on the wall farthest from the audience
+ *
+ * A final calculation then uses the location of the camera on the robot to determine the
+ * robot's location and orientation on the field.
+ */
+
+public class VuforiaEncapsulator {
+    private static String VUFORIA_KEY;
+
+    static {
+        Properties vuforiaProps = new Properties();
+        InputStream vuforiaPropsFile = VuforiaEncapsulator.class.getClassLoader().getResourceAsStream("vuforia.properties");
+
+        try {
+            vuforiaProps.load(vuforiaPropsFile);
+        } catch (IOException e) {
+            Log.w("team-code", "Unable to load vuforia properties file", e);
+        }
+
+        VUFORIA_KEY = vuforiaProps.getProperty("VUFORIA_KEY");
+    }
+
+    // Since ImageTarget trackables use mm to specifiy their dimensions, we must use mm for all the physical dimension.
+    // We will define some constants and conversions here
+    private static final float mmPerInch        = 25.4f;
+    private static final float mmFTCFieldWidth  = (12*6) * mmPerInch;       // the width of the FTC field (from the center point to the outer panels)
+    private static final float mmTargetHeight   = (6) * mmPerInch;          // the height of the center of the target image above the floor
+
+    // Select which camera you want use.  The FRONT camera is the one on the same side as the screen.
+    // Valid choices are:  BACK or FRONT
+    private static VuforiaLocalizer.CameraDirection CAMERA_CHOICE = BACK;
+
+    private OpenGLMatrix lastLocation = null;
+    private boolean targetVisible = false;
+
+    /**
+     * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
+     * localization engine.
+     */
+    private VuforiaLocalizer vuforia;
+
+    private int cameraMonitorViewId;
+    private VuforiaLocalizer.Parameters parameters;
+    private VuforiaTrackables targetsRoverRuckus;
+    private VuforiaTrackable blueRover;
+    private VuforiaTrackable redFootprint;
+    private VuforiaTrackable frontCraters;
+    private VuforiaTrackable backSpace;
+    private List<VuforiaTrackable> allTrackables;
+    private OpenGLMatrix blueRoverLocationOnField;
+    private OpenGLMatrix redFootprintLocationOnField;
+    private OpenGLMatrix frontCratersLocationOnField;
+    private OpenGLMatrix backSpaceLocationOnField;
+    private OpenGLMatrix phoneLocationOnRobot;
+    private OpenGLMatrix robotLocationTransform;
+    private VectorF translation;
+    Orientation rotation;
+
+    // Camera location and rotation
+    private int CAMERA_FORWARD_DISPLACEMENT;   // eg: Camera is 110 mm in front of robot center
+    private int CAMERA_VERTICAL_DISPLACEMENT;   // eg: Camera is 200 mm above ground
+    private int CAMERA_LEFT_DISPLACEMENT;     // eg: Camera is ON the robot's center line
+
+    public VuforiaEncapsulator(String camera_side, int camera_forward_displace, int camera_vertical_displace, int camera_horizontal_displace, HardwareMap hardwareMap) {
+        if(camera_side.equals("FRONT")) {
+            CAMERA_CHOICE = FRONT;
+        } else {
+            CAMERA_CHOICE = BACK;
+        }
+
+        CAMERA_FORWARD_DISPLACEMENT = camera_forward_displace;
+        CAMERA_VERTICAL_DISPLACEMENT = camera_vertical_displace;
+        CAMERA_LEFT_DISPLACEMENT = camera_horizontal_displace;
+
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         * We can pass Vuforia the handle to a camera preview resource (on the RC phone);
+         * If no camera monitor is desired, use the parameterless constructor instead (commented out below).
+         */
+        cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY ;
+        parameters.cameraDirection   = CAMERA_CHOICE;
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        // Load the data sets that for the trackable objects. These particular data
+        // sets are stored in the 'assets' part of our application.
+        targetsRoverRuckus = this.vuforia.loadTrackablesFromAsset("RoverRuckus");
+        blueRover = targetsRoverRuckus.get(0);
+        blueRover.setName("Blue-Rover");
+        redFootprint = targetsRoverRuckus.get(1);
+        redFootprint.setName("Red-Footprint");
+        frontCraters = targetsRoverRuckus.get(2);
+        frontCraters.setName("Front-Craters");
+        backSpace = targetsRoverRuckus.get(3);
+        backSpace.setName("Back-Space");
+
+        // For convenience, gather together all the trackable objects in one easily-iterable collection */
+        allTrackables = new ArrayList<VuforiaTrackable>();
+        allTrackables.addAll(targetsRoverRuckus);
+
+        /*
+         * In order for localization to work, we need to tell the system where each target is on the field, and
+         * where the phone resides on the robot.  These specifications are in the form of <em>transformation matrices.</em>
+         * Transformation matrices are a central, important concept in the math here involved in localization.
+         * See <a href="https://en.wikipedia.org/wiki/Transformation_matrix">Transformation Matrix</a>
+         * for detailed information. Commonly, you'll encounter transformation matrices as instances
+         * of the {@link OpenGLMatrix} class.
+         *
+         * If you are standing in the Red Alliance Station looking towards the center of the field,
+         *     - The X axis runs from your left to the right. (positive from the center to the right)
+         *     - The Y axis runs from the Red Alliance Station towards the other side of the field
+         *       where the Blue Alliance Station is. (Positive is from the center, towards the BlueAlliance station)
+         *     - The Z axis runs from the floor, upwards towards the ceiling.  (Positive is above the floor)
+         *
+         * This Rover Ruckus sample places a specific target in the middle of each perimeter wall.
+         *
+         * Before being transformed, each target image is conceptually located at the origin of the field's
+         *  coordinate system (the center of the field), facing up.
+         */
+
+        /*
+         * To place the BlueRover target in the middle of the blue perimeter wall:
+         * - First we rotate it 90 around the field's X axis to flip it upright.
+         * - Then, we translate it along the Y axis to the blue perimeter wall.
+         */
+        blueRoverLocationOnField = OpenGLMatrix
+                .translation(0, mmFTCFieldWidth, mmTargetHeight)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 0));
+        blueRover.setLocation(blueRoverLocationOnField);
+
+        /*
+         * To place the RedFootprint target in the middle of the red perimeter wall:
+         * - First we rotate it 90 around the field's X axis to flip it upright.
+         * - Second, we rotate it 180 around the field's Z axis so the image is flat against the red perimeter wall
+         *   and facing inwards to the center of the field.
+         * - Then, we translate it along the negative Y axis to the red perimeter wall.
+         */
+        redFootprintLocationOnField = OpenGLMatrix
+                .translation(0, -mmFTCFieldWidth, mmTargetHeight)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 180));
+        redFootprint.setLocation(redFootprintLocationOnField);
+
+        /*
+         * To place the FrontCraters target in the middle of the front perimeter wall:
+         * - First we rotate it 90 around the field's X axis to flip it upright.
+         * - Second, we rotate it 90 around the field's Z axis so the image is flat against the front wall
+         *   and facing inwards to the center of the field.
+         * - Then, we translate it along the negative X axis to the front perimeter wall.
+         */
+        frontCratersLocationOnField = OpenGLMatrix
+                .translation(-mmFTCFieldWidth, 0, mmTargetHeight)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0 , 90));
+        frontCraters.setLocation(frontCratersLocationOnField);
+
+        /*
+         * To place the BackSpace target in the middle of the back perimeter wall:
+         * - First we rotate it 90 around the field's X axis to flip it upright.
+         * - Second, we rotate it -90 around the field's Z axis so the image is flat against the back wall
+         *   and facing inwards to the center of the field.
+         * - Then, we translate it along the X axis to the back perimeter wall.
+         */
+        backSpaceLocationOnField = OpenGLMatrix
+                .translation(mmFTCFieldWidth, 0, mmTargetHeight)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, -90));
+        backSpace.setLocation(backSpaceLocationOnField);
+
+        /*
+         * Create a transformation matrix describing where the phone is on the robot.
+         *
+         * The coordinate frame for the robot looks the same as the field.
+         * The robot's "forward" direction is facing out along X axis, with the LEFT side facing out along the Y axis.
+         * Z is UP on the robot.  This equates to a bearing angle of Zero degrees.
+         *
+         * The phone starts out lying flat, with the screen facing Up and with the physical top of the phone
+         * pointing to the LEFT side of the Robot.  It's very important when you test this code that the top of the
+         * camera is pointing to the left side of the  robot.  The rotation angles don't work if you flip the phone.
+         *
+         * If using the rear (High Res) camera:
+         * We need to rotate the camera around it's long axis to bring the rear camera forward.
+         * This requires a negative 90 degree rotation on the Y axis
+         *
+         * If using the Front (Low Res) camera
+         * We need to rotate the camera around it's long axis to bring the FRONT camera forward.
+         * This requires a Positive 90 degree rotation on the Y axis
+         *
+         * Next, translate the camera lens to where it is on the robot.
+         * In this example, it is centered (left to right), but 110 mm forward of the middle of the robot, and 200 mm above ground level.
+         */
+
+        phoneLocationOnRobot = OpenGLMatrix
+                .translation(CAMERA_FORWARD_DISPLACEMENT, CAMERA_LEFT_DISPLACEMENT, CAMERA_VERTICAL_DISPLACEMENT)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, YZX, DEGREES,
+                        CAMERA_CHOICE == FRONT ? 90 : -90, 0, 0));
+        translation = new VectorF(0,0,0);
+        rotation = new Orientation();
+    }
+
+    public void init() {
+        /*  Let all the trackable listeners know where the phone is.  */
+        for (VuforiaTrackable trackable : allTrackables)
+        {
+            ((VuforiaTrackableDefaultListener)trackable.getListener()).setPhoneInformation(phoneLocationOnRobot, parameters.cameraDirection);
+        }
+
+        /* Start tracking the data sets we care about. */
+        targetsRoverRuckus.activate();
+    }
+
+    public void update() {
+        // check all the trackable target to see which one (if any) is visible.
+        targetVisible = false;
+        for (VuforiaTrackable trackable : allTrackables) {
+            if (((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible()) {
+                targetVisible = true;
+
+                // getUpdatedRobotLocation() will return null if no new information is available since
+                // the last time that call was made, or if the trackable is not currently visible.
+                robotLocationTransform = ((VuforiaTrackableDefaultListener)trackable.getListener()).getUpdatedRobotLocation();
+                if (robotLocationTransform != null) {
+                    lastLocation = robotLocationTransform;
+                }
+                break;
+            }
+        }
+
+        // Provide feedback as to where the robot is located (if we know).
+        if (targetVisible) {
+            // express position (translation) of robot in inches.
+            translation = lastLocation.getTranslation();
+
+            // express the rotation of the robot in degrees.
+            rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
+        }
+    }
+
+    // Return X, Y, and Z Translations Individually
+    public float getX() {
+        return translation.get(0) / mmPerInch;
+    }
+    public float getY() {
+        return translation.get(1) / mmPerInch;
+    }
+    public float getZ() {
+        return translation.get(2) / mmPerInch;
+    }
+
+    // Return a Translation Vector
+    public Vector<Float> getTranslation() {
+        Vector<Float> pos = new Vector<>();
+        pos.addElement(getX());
+        pos.addElement(getY());
+        pos.addElement(getZ());
+        return pos;
+    }
+
+    // Return Pitch, Roll, and Heading Rotations Individually
+    public float getRoll() {
+        return rotation.firstAngle;
+    }
+    public float getPitch() {
+        return rotation.secondAngle;
+    }
+    public float getHeading() {
+        return rotation.thirdAngle;
+    }
+
+    // Get a Rotation Vector
+    public Vector<Float> getRotation() {
+        Vector<Float> rot = new Vector<>();
+        rot.addElement(getRoll());
+        rot.addElement(getPitch());
+        rot.addElement(getHeading());
+        return rot;
+    }
+
+    // Return the position and rotation as a string
+    public String getPositionString() {
+        if(translation==null || rotation==null){
+            return "unknown";
+        }
+        return "Position: " + getX() + ", " + getY() + ", " + getZ() + " | Rotation: " + getRoll() + ", " + getPitch() + ", " + getHeading();
+    }
+
+    public ClassHolder getSensors(OpModeExtended context) {
+        VuforiaEncapsulator encapsulator = this;
+
+        return new ClassHolder(context) {
+            @Override
+            public Map<String, Subsystem> getSubsystems() {
+                return new HashMap<>();
+            }
+
+            @Override
+            public Map<String, SensorManager> getSensors() {
+                Map<String, SensorManager> res = new HashMap<>();
+
+                for (int i = 0; i < 6; ++i) {
+                    SensorManager manager =  new SensorManager(context, new VuforiaSensor(encapsulator, i));
+                    res.put(manager.sensorInterface.getConfigName(), manager);
+                }
+
+                return res;
+            }
+
+            @Override
+            public Map<String, JavaInterface> getInterfaces() {
+                return new HashMap<>();
+            }
+        };
+    }
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/vuforia/VuforiaFacade.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode.core.vuforia;
+
+import org.firstinspires.ftc.teamcode.core.Registry;
+import org.firstinspires.ftc.teamcode.core.SensorManager;
+
+import java.util.Vector;
+
+public class VuforiaFacade {
+    private SensorManager x, y, z, pitch, roll, heading;
+
+    public VuforiaFacade() {
+        this.x = Registry.getSensorManagerByName("VuforiaX");
+        this.y = Registry.getSensorManagerByName("VuforiaY");
+        this.z = Registry.getSensorManagerByName("VuforiaZ");
+        this.pitch = Registry.getSensorManagerByName("VuforiaPitch");
+        this.roll = Registry.getSensorManagerByName("VuforiaRoll");
+        this.heading = Registry.getSensorManagerByName("VuforiaHeading");
+    }
+
+    public double getX() {
+        return x.getCM();
+    }
+
+    public double getY() {
+        return y.getCM();
+    }
+
+    public double getZ() {
+        return z.getCM();
+    }
+
+    public Vector<Double> getTranslation() {
+        Vector<Double> pos = new Vector<>();
+        pos.addElement(getX());
+        pos.addElement(getY());
+        pos.addElement(getZ());
+        return pos;
+    }
+
+    public double getRoll() {
+        return roll.getCM();
+    }
+
+    public double getPitch() {
+        return pitch.getCM();
+    }
+
+    public double getHeading() {
+        return heading.getCM();
+    }
+
+    public Vector<Double> getRotation() {
+        Vector<Double> rot = new Vector<>();
+        rot.addElement(getRoll());
+        rot.addElement(getPitch());
+        rot.addElement(getHeading());
+        return rot;
+    }
+}
