b26c7737a7fbfa391c6c8e53d9d9c79c5046e4b6
gpsanant <gpsanant@gmail.com>
2018-12-13 16:28:34 -0800

    ADDED REGRESSION STUFF SUGMA

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/SensorStdDevTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/SensorStdDevTest.java
@@ -3,52 +3,76 @@ package org.firstinspires.ftc.teamcode.core;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.apache.commons.math3.stat.regression.SimpleRegression;
 import org.apache.commons.math3.stat.regression.RegressionResults;
 
 import java.util.ArrayList;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@TeleOp(name="SensorStdDevTest", group="Iterative Opmode")
-// This opmode gets the standard deviation of a sensor by
-// constantly replacing voltages in a list consisting of 100 values
-public class SensorStdDevTest extends OpMode {
-    static final int NUM_VOLTAGES = 100;
-    AnalogInput ultrasonic;
-    double[] voltageList;
-    double sum;
-    double mean;
-    double stdDev;
-    double[][] x;
-    SimpleRegression regression;
-    int i;
-
-    @Override
-    public void init() {
-        ultrasonic = hardwareMap.analogInput.get("ultra");
+import org.firstinspires.ftc.teamcode.balldrive.DriveSubsystem;
+import org.firstinspires.ftc.teamcode.balldrive.LiftSubsystem;
+import org.firstinspires.ftc.teamcode.core.structure.ClassHolder;
+import org.firstinspires.ftc.teamcode.core.OpModeExtended;
+import org.firstinspires.ftc.teamcode.core.structure.Registry;
+import org.firstinspires.ftc.teamcode.opmodes.Holder;
 
 
-        SimpleRegression regression = new SimpleRegression();
+@Autonomous(name = "SensorStdDevTest", group = "tele")
+public class SensorStdDevTest extends OpModeExtended {
 
-
+    public OpModeExtended.InputControlManager getInputControlManager() {
+        return new org.firstinspires.ftc.teamcode.core.SensorStdDevTest.AICM();
     }
 
-    @Override
-    public void loop() {
-        double currentVoltage = ultrasonic.getVoltage();
+    public ClassHolder getClassHolder() {
+        return new Holder(this);
+    }
+
+    public class AICM extends OpModeExtended.AutoInputControlManager {
+        DriveSubsystem drive;
+        LiftSubsystem lift;
+
+        static final int NUM_VOLTAGES = 100;
+        AnalogInput ultrasonic;
+        double[] voltageList;
+        double sum;
+        double mean;
+        double stdDev;
+        double[][] x;
+        ElapsedTime timer = new ElapsedTime();
+        SimpleRegression regression;
+        int i;
+
+        @Override
+        public void autoinit() {
+            ultrasonic = hardwareMap.analogInput.get("ultra");
+            timer.reset();
 
 
-        if (voltageList.length < NUM_VOLTAGES) {
-            voltageList[i] = currentVoltage;
-            x[i][0] = i;
-            i++;
-        } else {
-            regression.addObservations(x, voltageList);
-            RegressionResults lol = regression.regress();
-            telemetry.addData("hi brenda:", Math.sqrt(lol.getErrorSumSquares()/lol.getN()));
+            SimpleRegression regression = new SimpleRegression();
+            drive.setting("autoL", 1.0);
+            drive.setting("autoR", 1.0);
+
+
         }
 
+        @Override
+        public void autoupdate() {
+            double currentVoltage = ultrasonic.getVoltage();
 
+
+            if (voltageList.length < NUM_VOLTAGES) {
+                voltageList[i] = currentVoltage;
+                x[i][0] = timer.milliseconds();
+                i++;
+            } else {
+                regression.addObservations(x, voltageList);
+                RegressionResults lol = regression.regress();
+                telemetry.addData("hi brenda:", Math.sqrt(lol.getErrorSumSquares() / lol.getN()));
+                telemetry.addData("aaron:", lol.getAdjustedRSquared());
+            }
 
 
             telemetry.addData("Voltage", currentVoltage);
@@ -56,5 +80,5 @@ public class SensorStdDevTest extends OpMode {
             telemetry.update();
         }
     }
-
+}
 
