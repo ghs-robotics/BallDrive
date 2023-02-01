ea4b35fe5899c0e44795ddc9b2aff35afc51ec3b
zhaom <zhaomanyi2005@gmail.com>
2023-01-31 23:14:28 -0800

    added the basic code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/structure/SensorInterface.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.core.structure;
-
-public interface SensorInterface {
-    void init();
-    void update();
-    double getCMValue();
-    double getRawValue();
-    String getConfigName();
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/structure/Setting.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.core.structure;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Setting {
-
-}
